941183b21f2371fbbb63961ee5421424afec4e24
mkskh <maksym.kharchenko@dci-student.org>
2023-12-31 00:48:46 +0100

    button Pause Play Stop;Add song/songs;  - finished

--- main.py
+++ main.py
@@ -19,7 +19,21 @@ def play():
 
 def stop():
     pygame.mixer.music.stop()
-    songs_list.selection_clear(ACTIVE)
+
+global pause_status
+pause_status = False
+
+def pause(is_paused):
+    global pause_status
+    pause_status = is_paused
+
+    if pause_status:
+        pygame.mixer.music.unpause()
+        pause_status = False
+    else:
+        pygame.mixer.music.pause()
+        pause_status = True
+
 
 def add_song():
     song = filedialog.askopenfilename(initialdir='audio/', title='Add Song', filetypes=(('mp3 Files', '*.mp3'), ))
@@ -28,6 +42,15 @@ def add_song():
 
     songs_list.insert(END, song)
 
+
+def add_many_songs():
+    songs = filedialog.askopenfilenames(initialdir='audio/', title='Add Many Songs', filetypes=(('mp3 Files', '*.mp3'), ))
+
+    for song in songs:
+        song = song.replace('/home/dci-student/all/ON_GITHUB/GUI-mp3-player/audio/', '')
+        song = song.replace('.mp3', '')
+
+        songs_list.insert(END, song)
 
 
 songs_list = Listbox(root, bg='black', fg='white', width=60)
@@ -44,7 +67,7 @@ back_btn_img = PhotoImage(file='images/back.png')
 
 play_button = Button(frame, image=play_btn_img, command=play)
 stop_button = Button(frame, image=stop_btn_img, command=stop)
-pause_button = Button(frame, image=pause_btn_img)
+pause_button = Button(frame, image=pause_btn_img, command= lambda: pause(pause_status))
 next_button = Button(frame, image=next_btn_img)
 back_button = Button(frame, image=back_btn_img)
 
@@ -60,5 +83,6 @@ root.config(menu=menubar)
 file_menu = Menu(menubar, tearoff=0)
 menubar.add_cascade(label='File', menu=file_menu)
 file_menu.add_command(label='Add Song', command=add_song)
+file_menu.add_command(label='Add Many Songs', command=add_many_songs)
 
 root.mainloop()
