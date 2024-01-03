c81485281e76cb67fb86d23455b8ae45d887a040
mkskh <maksym.kharchenko@dci-student.org>
2024-01-03 01:27:02 +0100

    added next and back buttons. added Delete Menu

--- main.py
+++ main.py
@@ -53,6 +53,46 @@ def add_many_songs():
         songs_list.insert(END, song)
 
 
+def next():
+    next_song = songs_list.curselection()
+    next_song = next_song[0]+1
+
+    song = songs_list.get(next_song)
+    song = f'/home/dci-student/all/ON_GITHUB/GUI-mp3-player/audio/{song}.mp3'
+
+    pygame.mixer.music.load(song)
+    pygame.mixer.music.play()
+
+    songs_list.selection_clear(0, 'end')
+    songs_list.activate(next_song)
+    songs_list.selection_set(next_song)
+
+
+def back():
+    next_song = songs_list.curselection()
+    next_song = next_song[0]-1
+
+    song = songs_list.get(next_song)
+    song = f'/home/dci-student/all/ON_GITHUB/GUI-mp3-player/audio/{song}.mp3'
+
+    pygame.mixer.music.load(song)
+    pygame.mixer.music.play()
+
+    songs_list.selection_clear(0, 'end')
+    songs_list.activate(next_song)
+    songs_list.selection_set(next_song)
+
+
+def delete_song():
+    songs_list.delete(ANCHOR)
+    pygame.mixer.music.stop()
+
+
+def delete_all_songs():
+    songs_list.delete(0, 'end')
+    pygame.mixer.music.stop()
+
+
 songs_list = Listbox(root, bg='black', fg='white', width=60)
 songs_list.pack(pady=15)
 
@@ -65,11 +105,11 @@ pause_btn_img = PhotoImage(file='images/pause.png')
 next_btn_img = PhotoImage(file='images/next.png')
 back_btn_img = PhotoImage(file='images/back.png')
 
-play_button = Button(frame, image=play_btn_img, command=play)
-stop_button = Button(frame, image=stop_btn_img, command=stop)
-pause_button = Button(frame, image=pause_btn_img, command= lambda: pause(pause_status))
-next_button = Button(frame, image=next_btn_img)
-back_button = Button(frame, image=back_btn_img)
+play_button = Button(frame, image=play_btn_img, borderwidth=0, command=play)
+stop_button = Button(frame, image=stop_btn_img, borderwidth=0, command=stop)
+pause_button = Button(frame, image=pause_btn_img, borderwidth=0, command= lambda: pause(pause_status))
+next_button = Button(frame, image=next_btn_img, borderwidth=0, command=next)
+back_button = Button(frame, image=back_btn_img, borderwidth=0, command=back)
 
 play_button.grid(row=0, column=2, padx=10)
 stop_button.grid(row=0, column=1, padx=10)
@@ -84,5 +124,10 @@ file_menu = Menu(menubar, tearoff=0)
 menubar.add_cascade(label='File', menu=file_menu)
 file_menu.add_command(label='Add Song', command=add_song)
 file_menu.add_command(label='Add Many Songs', command=add_many_songs)
+
+delete_menu = Menu(menubar, tearoff=0)
+menubar.add_cascade(label='Delete', menu=delete_menu)
+delete_menu.add_command(label='Delete Current Song', command=delete_song)
+delete_menu.add_command(label='Delete All Songs', command=delete_all_songs)
 
 root.mainloop()
