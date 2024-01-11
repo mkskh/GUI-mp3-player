a25a126f30d87a7a5f29b030658f52730e4a636a
mkskh <maksym.kharchenko@dci-student.org>
2024-01-11 01:43:56 +0100

    finished mp3 player

--- main.py
+++ main.py
@@ -5,14 +5,17 @@ import time
 from mutagen.mp3 import MP3
 import tkinter.ttk as ttk
 
+
 root = Tk()
 root.title('mp3 player')
-root.geometry('500x420')
+root.geometry('500x430')
 
 pygame.mixer.init()
 
 
 def play():
+    global stopped
+    stopped = False
     song = songs_list.get(ACTIVE)
     song = f'/home/dci-student/all/ON_GITHUB/GUI-mp3-player/audio/{song}.mp3'
 
@@ -21,15 +24,22 @@ def play():
 
     play_duration()
 
-    slider_position = int(song_length)
-    my_slider.config(to=slider_position, value=0)
+
+global stopped
+stopped = False
 
 
 def stop():
+    duration_box.config(text='')
+    my_slider.config(value=0)
+
     pygame.mixer.music.stop()
     songs_list.selection_clear(ACTIVE)
 
     duration_box.config(text='')
+
+    global stopped
+    stopped = True
 
 global pause_status
 pause_status = False
@@ -66,6 +76,10 @@ def add_many_songs():
 
 
 def next():
+
+    duration_box.config(text='')
+    my_slider.config(value=0)
+
     next_song = songs_list.curselection()
     next_song = next_song[0]+1
 
@@ -81,6 +95,10 @@ def next():
 
 
 def back():
+
+    duration_box.config(text='')
+    my_slider.config(value=0)
+
     next_song = songs_list.curselection()
     next_song = next_song[0]-1
 
@@ -96,42 +114,73 @@ def back():
 
 
 def delete_song():
+    stop()
     songs_list.delete(ANCHOR)
     pygame.mixer.music.stop()
 
 
 def delete_all_songs():
+    stop()
     songs_list.delete(0, 'end')
     pygame.mixer.music.stop()
 
 
 def play_duration():
+
+    if stopped:
+        return
+    
     current_time = pygame.mixer.music.get_pos() / 1000
     converted_time_duration = time.strftime('%M:%S', time.gmtime(current_time))
 
-    if pygame.mixer.music.get_busy():
-        position = songs_list.curselection()
-        if position:
-            position = position[0]
-            song = songs_list.get(position)
-            song = f'/home/dci-student/all/ON_GITHUB/GUI-mp3-player/audio/{song}.mp3'
+    song = songs_list.get(ACTIVE)
+    song = f'/home/dci-student/all/ON_GITHUB/GUI-mp3-player/audio/{song}.mp3'
 
-            song_mut = MP3(song)
-            global song_length
-            song_length = song_mut.info.length
-            converted_general_time = time.strftime('%M:%S', time.gmtime(song_length))
-    
-            duration_box.config(text=f'{converted_time_duration} / {converted_general_time}')
+    song_mut = MP3(song)
+    global song_length
+    song_length = song_mut.info.length
+    converted_general_time = time.strftime('%M:%S', time.gmtime(song_length))
+
+    current_time += 1
+
+    if int(my_slider.get()) == int(song_length):
+        duration_box.config(text=f'{converted_general_time} / {converted_general_time}')
+        next()
+
+    elif pause_status:
+        pass
+
+    elif int(my_slider.get()) == int(current_time):
+        slider_position = int(song_length)
+        my_slider.config(to=slider_position, value=int(current_time))
+        # duration_box.config(text=f'{converted_time_duration} / {converted_general_time}')
     else:
-        duration_box.config(text='')
+        slider_position = int(song_length)
+        my_slider.config(to=slider_position, value=int(my_slider.get()))
 
-    my_slider.config(value=int(current_time))
+        converted_time_duration = time.strftime('%M:%S', time.gmtime(int(my_slider.get())))
+
+        duration_box.config(text=f'{converted_time_duration} / {converted_general_time}')
+
+        next_time = int(my_slider.get()) + 1
+        my_slider.config(value=next_time)
 
     duration_box.after(1000, play_duration)
 
 
 def slide(x):
-    slider_label.config(text=f'{int(my_slider.get())} of {int(song_length)}')
+    # slider_label.config(text=f'{int(my_slider.get())} of {int(song_length)}')
+
+    song = songs_list.get(ACTIVE)
+    song = f'/home/dci-student/all/ON_GITHUB/GUI-mp3-player/audio/{song}.mp3'
+
+    pygame.mixer.music.load(song)
+    pygame.mixer.music.play(loops=0, start=int(my_slider.get()))
+
+
+def volume(x):
+    pygame.mixer.music.set_volume(volume_box.get())
+
 
 
 songs_list = Listbox(root, bg='black', fg='white', width=60)
@@ -174,10 +223,13 @@ delete_menu.add_command(label='Delete All Songs', command=delete_all_songs)
 duration_box = Label(root, text='', bd=1, relief=GROOVE, anchor=E)
 duration_box.pack(fill=X, side=BOTTOM, ipady=2)
 
-my_slider = ttk.Scale(root, from_=0, to=100, orient=HORIZONTAL, value=0, command=slide, length=360)
+my_slider = ttk.Scale(root, from_=0, to=100, orient=HORIZONTAL, value=0, command=slide, length=430)
 my_slider.pack(pady=20)
 
-slider_label = Label(root, text='0')
-slider_label.pack(pady=10)
+volume_label = Label(root, text='Volume:')
+volume_label.pack(pady=5, padx=7, side=LEFT)
+
+volume_box = ttk.Scale(root, from_=0, to=1, orient=HORIZONTAL, value=1, command=volume, length=110)
+volume_box.pack(pady=5, padx=7, side=LEFT)
 
 root.mainloop()
